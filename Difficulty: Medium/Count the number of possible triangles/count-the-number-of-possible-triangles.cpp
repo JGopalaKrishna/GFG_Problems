fdc99e847bb36ae35aeb175ca337a3a47127bcee
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2025-08-27 10:09:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,57 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to count the number of possible triangles.
     int countTriangles(vector<int>& arr) {
-        // code here
-        int n=arr.size();
-        if(n<3) return 0;
-         sort(arr.begin(),arr.end());
-         int count=0;
-         for(int k=2;k<n;++k){
-             int i=0,j=k-1;
-             while(i<j){
-                 if(arr[i]+arr[j]>arr[k]){
-                     count +=(j-i);
-                     --j;
-                 }else{
-                     ++i;
-                 }
-             }
-         }
-    return count;
+        
+        int count  = 0;
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        
+        for(int i = n - 1; i >= 2; i--)
+        {
+            int left = 0, right = i - 1;
+            while(left < right)
+            {
+                if(arr[left] + arr[right] > arr[i])
+                {
+                    count += (right - left);
+                    right--;
+                }else left++;
+            }
+        }
+        return count;
     }
 };
 
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
+//GFG POTD solution for 27 August
